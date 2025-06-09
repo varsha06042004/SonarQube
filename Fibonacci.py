24a0a64be2d6ac7226efad60c154f2cdc9683712
varsha06042004 <varsha.kalahasti@accenture.com>
2025-06-09 16:13:12 +0530

    Update Fibonacci.py
    duplicates

--- Fibonacci.py
+++ Fibonacci.py
@@ -16,6 +16,25 @@ def fibonacci(n)
         sequence.append(next_num)
     return sequence
 
+def fibonacci(n)
+    """Generate a list of the first n Fibonacci numbers."""
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    sequence = [0, 1]
+    for i in range(2, n):
+        next_num = sequence[i - 1] + sequence[i - 2]
+        sequence.append(next_num)
+    return sequence
+
+
+
+
+
 if __name__ == "__main__":
     try:
         num = int(input("Enter the number of Fibonacci terms: "))
